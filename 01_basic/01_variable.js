72b49ff9105e7ea534399b3e6830a86d83f58f63
Jimit0111 <jimitprajapati812@gmail.com>
2025-11-17 05:29:17 +0000

    datatypes

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,13 @@
+const studentName="jimit"
+let studentCity="visnagar"
+var studentGrade="A"
+
+
+console.log(studentCity)
+console.log(studentName)
+console.log(studentGrade)
+
+
+
+
+console.table([studentCity,studentGrade,studentName])
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,11 @@
+//number (range 2 power 53)
+//big int 
+//string ""
+//boolean = true/false
+//null = standalone 
+// undefined => 
+// symbol => unique
+//object
+
+
+console.log(typeof "jimit")
