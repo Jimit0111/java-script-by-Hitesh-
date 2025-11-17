d9b1cb2af708713c53810d4bf112251febbac4f9
Jimit0111 <jimitprajapati812@gmail.com>
2025-11-17 11:20:35 +0000

    conversion

---
+++ 01_basic/03_conversionoperation.js
@@ -0,0 +1,18 @@
+let score= true
+
+console.log(typeof score)
+    
+let isLoggedIn=0
+
+let booleanIsLoggedIn = Boolean(
+    isLoggedIn
+)
+
+console.log(booleanIsLoggedIn)
+
+//"33" => 33
+//"33abc" => Nan
+
+// true => 1 , false => 0 
+//"" => false 
+//"hitsh" => true
