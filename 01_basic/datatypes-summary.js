ea5d2403275994b8077b865a3579b6c7a8d3d1ed
Jimit0111 <jimitprajapati812@gmail.com>
2025-11-19 15:53:11 +0000

    datatypes

---
+++ 01_basic/datatypes-summary.js
@@ -0,0 +1,20 @@
+// primitive 
+
+// 7 types : string, number, boolean, null , undefined , symbol , Bigint
+
+let number=333;
+const name= "jimit"
+
+const bigNumber= 774757574n  
+
+
+// Reference  (Non primitive )  
+
+// Array , object , functions
+
+const heros=["saktiman ", " krishna ", "ram"];
+let myObject=
+{
+    name:"jimit",
+    city:"pln"
+}
